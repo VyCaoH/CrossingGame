9a28821871bd4aa5d42b609bc979d44f9e90610d
Huy203 <95161186+Huy203@users.noreply.github.com>
2022-11-23 16:48:15 +0700

    add draw_entities

--- Renderer.h
+++ Renderer.h
@@ -1,6 +1,7 @@
 
 typedef int s32;
 typedef unsigned int u32;
+enum TYPE { CAR, TRUCK, BIRD, DINOSAUR };
 
 inline int clamp(int min, int val, int max)
 {
@@ -37,7 +38,7 @@ draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
 static float render_scale = 0.01f;
 
 static void
-draw_arena_borders(float x, float y,float arena_x, float arena_y, u32 color) {
+draw_arena_borders(float x, float y, float arena_x, float arena_y, u32 color) {
 	x *= render_state.height * render_scale;
 	y *= render_state.height * render_scale;
 	arena_x *= render_state.height * render_scale;
@@ -82,7 +83,7 @@ draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
 	draw_rect_in_pixels((int)x0, (int)y0, (int)x1, (int)y1, color);
 }
 static void
-draw_borders(float x1,float y1,float half_size_x1, float half_size_y1, float x2, float y2, float half_size_x2, float half_size_y2, u32 color) {
+draw_borders(float x1, float y1, float half_size_x1, float half_size_y1, float x2, float y2, float half_size_x2, float half_size_y2, u32 color) {
 	x1 *= render_state.height * render_scale;
 	y1 *= render_state.height * render_scale;
 	x1 += render_state.width / 2.f;
@@ -111,11 +112,19 @@ draw_borders(float x1,float y1,float half_size_x1, float half_size_y1, float x2,
 
 static bool checkWall(float x)
 {
-	x*= render_state.height * render_scale;
-	x+= render_state.width / 2.f;
+	x *= render_state.height * render_scale;
+	x += render_state.width / 2.f;
 	if (x > 0)
 		return true;
 	return false;
+}
+static void draw_lane(float y, float half_size_x, float half_size_y, u32 color)
+{
+	float x0 = render_state.width / 2 - (render_state.width / 2 - half_size_x);
+	float y0 = render_state.height / 2 - (render_state.height / 2 - half_size_x);
+
+	draw_rect(0, y, x0, 0.8, 0x9C9900);
+	//draw_rect(0, y-0.2, render_state.width / 2, 0.5, 0x33AA00);
 }
 //x: trung tam dino, y: trung taam dino
 static void draw_truck(float x, float y, float half_size_x, float half_size_y)
@@ -124,8 +133,8 @@ static void draw_truck(float x, float y, float half_size_x, float half_size_y)
 	draw_rect(x, y + 1, 3.5, 2, 0xfff0000);
 
 	draw_rect(x, y - 0.5, 5, 1.5, 0x1820FAA);
-	draw_rect(x+2.5, y-2.5 , 1, 1, 0x1820FAA);
-	draw_rect(x -1.5, y - 2.5, 1, 1, 0x1820FAA);
+	draw_rect(x + 2.5, y - 2.5, 1, 1, 0x1820FAA);
+	draw_rect(x - 1.5, y - 2.5, 1, 1, 0x1820FAA);
 
 	draw_rect(x + 2, y + 1, 2.5, 3, 0x1820FF);
 	draw_rect(x + 2, y + 1, 3, 2.5, 0x1820FF);
@@ -133,50 +142,50 @@ static void draw_truck(float x, float y, float half_size_x, float half_size_y)
 }
 static void draw_dino(float x, float y, float half_size_x, float half_size_y)
 {
-	draw_rect(x + 3,y + 0.7, 2, 0.2, 0x000000);
-	draw_rect(x,y - 3, 7 + 0.2, 1.5 + 0.2, 0x000000);
+	draw_rect(x + 3, y + 0.7, 2, 0.2, 0x000000);
+	draw_rect(x, y - 3, 7 + 0.2, 1.5 + 0.2, 0x000000);
 
-	draw_rect(x,y - 4.5, 2 + 0.2, 2 + 0.2, 0x000000);
-	draw_rect(x + 1,y - 4.5 - 2, 2 + 0.2, 0.5 + 0.2, 0x000000);
-	draw_rect(x + 2,y - 4.5 - 3, 2 + 0.2, 0.5 + 0.2, 0x000000);
-	draw_rect(x + 3,y - 4.5 - 4, 1.5 + 0.2, 0.5 + 0.2, 0x000000);
-	draw_rect(x + 4,y - 4.5 - 5, 1. + 0.2, 0.5 + 0.2, 0x000000);
-	draw_rect(x + 4.5,y - 4.5 - 5.5, 0.9 + 0.2, 0.1 + 0.5, 0x000000);
-
-
-	draw_rect(x + 3,y, 2, 0.5, 0x1820FF);
-	draw_rect(x + 2,y - 1, 2, 0.5, 0x1820FF);
-	draw_rect(x + 1,y - 2, 2, 0.5, 0x1820FF);
-	draw_rect(x,y - 3, 2, 0.5, 0x1820FF);
-
-	draw_rect(x,y - 3, 7, 1.5, 0x1820FF);
+	draw_rect(x, y - 4.5, 2 + 0.2, 2 + 0.2, 0x000000);
+	draw_rect(x + 1, y - 4.5 - 2, 2 + 0.2, 0.5 + 0.2, 0x000000);
+	draw_rect(x + 2, y - 4.5 - 3, 2 + 0.2, 0.5 + 0.2, 0x000000);
+	draw_rect(x + 3, y - 4.5 - 4, 1.5 + 0.2, 0.5 + 0.2, 0x000000);
+	draw_rect(x + 4, y - 4.5 - 5, 1. + 0.2, 0.5 + 0.2, 0x000000);
+	draw_rect(x + 4.5, y - 4.5 - 5.5, 0.9 + 0.2, 0.1 + 0.5, 0x000000);
 
 
-	draw_rect(x,y - 4.5, 2, 2, 0x1820FF);
-	draw_rect(x + 1,y - 4.5 - 2, 2, 0.5, 0x1820FF);
-	draw_rect(x + 2,y - 4.5 - 3, 2, 0.5, 0x1820FF);
-	draw_rect(x + 3,y - 4.5 - 4, 1.5, 0.5, 0x1820FF);
-	draw_rect(x + 4,y - 4.5 - 5, 1., 0.5, 0x1820FF);
-	draw_rect(x + 4.5,y - 4.5 - 5.5, 0.9, 0.1, 0x1820FF);
+	draw_rect(x + 3, y, 2, 0.5, 0x1820FF);
+	draw_rect(x + 2, y - 1, 2, 0.5, 0x1820FF);
+	draw_rect(x + 1, y - 2, 2, 0.5, 0x1820FF);
+	draw_rect(x, y - 3, 2, 0.5, 0x1820FF);
+
+	draw_rect(x, y - 3, 7, 1.5, 0x1820FF);
+
+
+	draw_rect(x, y - 4.5, 2, 2, 0x1820FF);
+	draw_rect(x + 1, y - 4.5 - 2, 2, 0.5, 0x1820FF);
+	draw_rect(x + 2, y - 4.5 - 3, 2, 0.5, 0x1820FF);
+	draw_rect(x + 3, y - 4.5 - 4, 1.5, 0.5, 0x1820FF);
+	draw_rect(x + 4, y - 4.5 - 5, 1., 0.5, 0x1820FF);
+	draw_rect(x + 4.5, y - 4.5 - 5.5, 0.9, 0.1, 0x1820FF);
 
 	//draw_rect_in_pixels((int)x0, (int)y0, (int)x1, (int)y1, 0x1820FF);
 }
 static const char* letters[][7] = {
 	" 00",
-	"|  0",
-	"|  0",
-	"|000",
-	"|  0",
-	"|  0",
-	"|  0",
+	"0  0",
+	"0  0",
+	"0000",
+	"0  0",
+	"0  0",
+	"0  0",
 
-	"|00",
-	"|  0",
-	"|  0",
-	"|00",
-	"|  0",
-	"|  0",
-	"|00",
+	"000",
+	"0  0",
+	"0  0",
+	"000",
+	"0  0",
+	"0  0",
+	"000",
 
 	" 000",
 	"0",
@@ -416,6 +425,227 @@ draw_text(const char* text, float x, float y, float size, u32 color) {
 		text++;
 		x += size * 6.f;
 		y = original_y;
+	}
+}//123456789
+static const char* entities[][14] = {
+	//car
+	"0000000000111111110",
+	"0000000001111111111",
+	"0000000001111111111",
+	"0000002221111111111",
+	"0000023331111111111",
+	"0000233331111111111",
+	"0000233331111111111",
+	"0444444441111111111",
+	"4444444441111111111",
+	"5544424441111121111",
+	"4444222440111222110",
+	"0002262200002262200",
+	"0000222000000222000",
+	"0000020000000020000",
+
+	//Truck
+	"0000000000111111110",
+	"0000000001111111111",
+	"0000000001111111111",
+	"0000002221111111111",
+	"0000023331111111111",
+	"0000233331111111111",
+	"0000233331111111111",
+	"0444444441111111111",
+	"4444444441111111111",
+	"5544424441111121111",
+	"4444222440111222110",
+	"0002262200002262200",
+	"0000222000000222000",
+	"0000020000000020000",
+
+	//bird
+
+
+	//dino
+
+
+};
+static void draw_entities(int number, float x, float y, float size, u32 color)
+{
+	float half_size = size * .5f;
+	float original_y = y;
+	switch (number)
+	{
+	case CAR:
+	{
+		break;
+
+	}
+	case TRUCK:
+	{
+		const char** entity;
+		/*if (*text == 47) entity = entities[27];
+		else if (*text == 46) entity = entities[26];*/
+		entity = entities[number];
+		float original_x = x;
+
+		for (int i = 0; i <= 13; i++) {
+			const char* row = entity[i];
+			while (*row) {
+				switch (*row)
+				{
+				case '0':
+				{
+					draw_rect(x, y, half_size, half_size, 0xFFFFFF);
+					break;
+				}
+				case '1':
+				{
+					draw_rect(x, y, half_size, half_size, 0x3366CC);
+					break;
+				}
+				case '2':
+				{
+					draw_rect(x, y, half_size, half_size, 0x000000);
+					break;
+				}
+				case '3':
+				{
+					draw_rect(x, y, half_size, half_size, 0xC9C9C9);
+					break;
+				}
+				case '4':
+				{
+					draw_rect(x, y, half_size, half_size, 0xFFD148);
+					break;
+				}
+				case '5':
+				{
+					draw_rect(x, y, half_size, half_size, 0xB3641F);
+					break;
+				}
+				case '6':
+				{
+					draw_rect(x, y, half_size, half_size, 0x3D3E59);
+					break;
+				}
+				case '7':
+				{
+					draw_rect(x, y, half_size, half_size, color);
+					break;
+				}
+				}
+
+				x += size;
+				row++;
+			}
+			y -= size;
+			x = original_x;
+		}
+		break;
+
+	}
+	case BIRD:
+	{
+		break;
+
+	}
+	case DINOSAUR:
+	{
+		break;
+
+	}
+	}
+}
+static void
+draw_number(int number, float x, float y, float size, u32 color) {
+	float half_size = size;// *.5f;
+
+	bool drew_number = false;
+	while (number || !drew_number) {
+		drew_number = true;
+
+		int digit = number % 10;
+		number = number / 10;
+
+		switch (digit) {
+		case 0: {
+			draw_rect(x - size, y, half_size, 2.5f * size, color);
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			draw_rect(x, y + size * 2.f, half_size, half_size, color);
+			draw_rect(x, y - size * 2.f, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 1: {
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			x -= size * 2.f;
+		} break;
+
+		case 2: {
+			draw_rect(x, y + size * 2.f, 1.5f * size, half_size, color);
+			draw_rect(x, y, 1.5f * size, half_size, color);
+			draw_rect(x, y - size * 2.f, 1.5f * size, half_size, color);
+			draw_rect(x + size, y + size, half_size, half_size, color);
+			draw_rect(x - size, y - size, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 3: {
+			draw_rect(x - half_size, y + size * 2.f, size, half_size, color);
+			draw_rect(x - half_size, y, size, half_size, color);
+			draw_rect(x - half_size, y - size * 2.f, size, half_size, color);
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 4: {
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			draw_rect(x - size, y + size, half_size, 1.5f * size, color);
+			draw_rect(x, y, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 5: {
+			draw_rect(x, y + size * 2.f, 1.5f * size, half_size, color);
+			draw_rect(x, y, 1.5f * size, half_size, color);
+			draw_rect(x, y - size * 2.f, 1.5f * size, half_size, color);
+			draw_rect(x - size, y + size, half_size, half_size, color);
+			draw_rect(x + size, y - size, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 6: {
+			draw_rect(x + half_size, y + size * 2.f, size, half_size, color);
+			draw_rect(x + half_size, y, size, half_size, color);
+			draw_rect(x + half_size, y - size * 2.f, size, half_size, color);
+			draw_rect(x - size, y, half_size, 2.5f * size, color);
+			draw_rect(x + size, y - size, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 7: {
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			draw_rect(x - half_size, y + size * 2.f, size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 8: {
+			draw_rect(x - size, y, half_size, 2.5f * size, color);
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			draw_rect(x, y + size * 2.f, half_size, half_size, color);
+			draw_rect(x, y - size * 2.f, half_size, half_size, color);
+			draw_rect(x, y, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+
+		case 9: {
+			draw_rect(x - half_size, y + size * 2.f, size, half_size, color);
+			draw_rect(x - half_size, y, size, half_size, color);
+			draw_rect(x - half_size, y - size * 2.f, size, half_size, color);
+			draw_rect(x + size, y, half_size, 2.5f * size, color);
+			draw_rect(x - size, y + size, half_size, half_size, color);
+			x -= size * 4.f;
+		} break;
+		}
+
 	}
 }
 //static void
